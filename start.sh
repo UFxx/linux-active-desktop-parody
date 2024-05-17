b4407b58e14af3e1ec46dbaf2ca8c9e18365dfd4
Dmitriy <87441804+UFxx@users.noreply.github.com>
2024-05-17 19:06:54 +0300

    Delete start.sh

--- start.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-FILE_PATH="Projects/Wallpapers/scripts/screenshot.py"
-
-while true; do
-  source .venv/bin/activate
-  python "$FILE_PATH"
-  variety --set /usr/share/backgrounds/code.png
-done
