0598ceb3f8aeceb3d2c72da3d7152f8c51e7c13a
Dmitriy <smileprost@gmail.com>
2025-06-08 15:34:59 +0300

    Fix week number

--- src/App.vue
+++ src/App.vue
@@ -8,11 +8,22 @@
 	const daysOfWeek = ['Понедельник', 'Вторник', 'Среда', 'Четверг', 'Пятница', 'Суббота', 'Воскресенье'];
 	const months = ['Январь', 'Февраль', 'Март', 'Апрель', 'Май', 'Июнь', 'Июль', 'Август', 'Сентябрь', 'Октябрь', 'Ноябрь', 'Декабрь'];
 
+	function getWeekNumber(d) {
+		const date = new Date(d);
+		date.setHours(0, 0, 0, 0);
+		// Thursday in current week decides the year
+		date.setDate(date.getDate() + 3 - (date.getDay() + 6) % 7);
+		// January 4 is always in week 1
+		const week1 = new Date(date.getFullYear(), 0, 4);
+		// Adjust to Thursday in week 1 and count number of weeks from date to week1
+		return 1 + Math.round(((date.getTime() - week1.getTime()) / 86400000 - 3 + (week1.getDay() + 6) % 7) / 7);
+	}
+
 	const clock = computed(() => {
 		const currentDate = date.value;
 		return {
 			year: currentDate.getFullYear(),
-			weekNumber: Math.ceil(((currentDate.getMonth() + 1) * 30) / 7),
+			weekNumber: getWeekNumber(currentDate),
 			month: `${months[currentDate.getMonth()]} (${currentDate.getMonth() + 1})`,
 			dayOfMonth: currentDate.getDate(),
 			dayOfWeek:
